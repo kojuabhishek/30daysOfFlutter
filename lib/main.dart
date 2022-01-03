e73c57f37d26871a63b25a7ede752091749a90a8
kojuabhishek <abhishekkoju@gmail.com>
2022-01-03 13:05:22 +0545

    themes extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'pages/home_page.dart';
@@ -15,14 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-      primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
-      
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context), 
+      darkTheme: MyTheme.darkTheme(context), 
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,8 +9,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         title: Text("Catalog App"),
-      ),
+      
+        ),
+        
+      
       body: Center(
         child: Container(
           child: Text("Welcome to $days days of Flutter by $name"),
